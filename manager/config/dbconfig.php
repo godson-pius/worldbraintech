fd5242eeafc341b4f8f282c8674f8405976e347c
godson-pius <40927597+godson-pius@users.noreply.github.com>
2021-03-06 14:59:12 +0100

    Update Dbconfig

--- manager/config/dbconfig.php
+++ manager/config/dbconfig.php
@@ -1,12 +1,18 @@
 <?php
 session_start();
 
-define('HOST', 'localhost');
+/* define('HOST', 'localhost');
 define('USERNAME', 'root');
 define('PASSWORD', '');
 define('DBNAME', 'world_bt');
 
-$link = mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
+$link = mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME); */
+
+$link = mysqli_connect("localhost",
+    "worlgmig_worldbrain",
+    "100%worldbrain",
+    "worlgmig_worldbrain"
+);
 
 require_once "functions.php";
 require_once 'user_functions.php';
