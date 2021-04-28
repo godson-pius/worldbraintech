506c90c6015267ea242e457f53a82e6d17824f7b
godson-pius <40927597+godson-pius@users.noreply.github.com>
2021-04-28 16:32:13 +0100

    UIpdated the index

--- index.php
+++ index.php
@@ -134,13 +134,13 @@ if (isset($_POST['submit'])){
     <div class="row">
         <!-- Team member-->
         <div class="col-lg-3 col-sm-6">
-<<<<<<< HEAD
+
           <div class="team-card-style-1 mx-auto">
             <div class="team-card-inner">
               <div class="team-thumb"><img src="img/team/style-1/Mr_Godson_The_CEO.jpeg" alt="Team Member Picture"/>
               </div>
               <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-facebook" href="#"><i class="socicon-facebook"></i></a><a class="social-btn sb-style-4 sb-twitter" href="#"><i class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-skype" href="#"><i class="socicon-skype"></i></a><a class="social-btn sb-style-4 sb-linkedin" href="#"><i class="socicon-linkedin"></i></a></div><a class="team-contact-link" href="tel:+72056557984"><i class="fe-icon-phone"></i>&nbsp;(+234) 803485976</a><a class="team-contact-link" href="mailto:info@example.com"><i class="fe-icon-mail"></i>&nbsp;worldbraintech@info.com</a>
-            </div><span class="team-position">Backend Experts & Graphic Designer</span>
+            </div><span class="team-position">CEO & Backend Expert</span>
             <h4 class="team-name">Godson Pius</h4>
           </div>
         </div>
@@ -162,62 +162,10 @@ if (isset($_POST['submit'])){
               <div class="team-thumb"><img src="img/team/style-1/Ani_Brendan.jpeg" alt="Team Member Picture"/>
               </div>
               <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-twitter" href="#"><i class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-github" href="#"><i class="socicon-github"></i></a><a class="social-btn sb-style-4 sb-stackoverflow" href="#"><i class="socicon-stackoverflow"></i></a><a class="social-btn sb-style-4 sb-skype" href="#"><i class="socicon-skype"></i></a></div><a class="team-contact-link" href="tel:+19871625346"><i class="fe-icon-phone"></i>&nbsp;(+234) 803485976</a><a class="team-contact-link" href="mailto:info@example.com"><i class="fe-icon-mail"></i>&nbsp;worldbraintech@info.com</a>
-            </div><span class="team-position">BlockChain Expert</span>
+            </div><span class="team-position">Backend Expert</span>
             <h4 class="team-name">Ani Brendan</h4>
           </div>
-=======
-            <div class="team-card-style-1 mx-auto">
-                <div class="team-card-inner">
-                    <div class="team-thumb"><img src="img/team/style-1/05.jpg" alt="Team Member Picture" />
-                    </div>
-                    <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-facebook" href="#"><i
-                                class="socicon-facebook"></i></a><a class="social-btn sb-style-4 sb-twitter" href="#"><i
-                                class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-skype" href="#"><i
-                                class="socicon-skype"></i></a><a class="social-btn sb-style-4 sb-linkedin" href="#"><i
-                                class="socicon-linkedin"></i></a></div><a class="team-contact-link"
-                        href="tel:+72056557984"><i class="fe-icon-phone"></i>&nbsp;(+234) 803485976</a><a
-                        class="team-contact-link" href="mailto:info@example.com"><i
-                            class="fe-icon-mail"></i>&nbsp;info@worldbraintechnology.com</a>
-                </div><span class="team-position">Backend Experts & Graphic Designer</span>
-                <h4 class="team-name">James Edmond</h4>
-            </div>
-        </div>
-        <!-- Team member-->
-        <div class="col-lg-3 col-sm-6">
-            <div class="team-card-style-1 mx-auto">
-                <div class="team-card-inner">
-                    <div class="team-thumb"><img src="img/team/style-1/06.jpg" alt="Team Member Picture" />
-                    </div>
-                    <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-facebook" href="#"><i
-                                class="socicon-facebook"></i></a><a class="social-btn sb-style-4 sb-twitter" href="#"><i
-                                class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-google-plus"
-                            href="#"><i class="socicon-googleplus"></i></a><a class="social-btn sb-style-4 sb-linkedin"
-                            href="#"><i class="socicon-linkedin"></i></a></div><a class="team-contact-link"
-                        href="tel:+57872336590"><i class="fe-icon-phone"></i>&nbsp;(+234) 812385976</a><a
-                        class="team-contact-link" href="mailto:info@example.com"><i
-                            class="fe-icon-mail"></i>&nbsp;info@worldbraintechnology.com</a>
-                </div><span class="team-position">Marketing Strategist</span>
-                <h4 class="team-name">Sara Madueke</h4>
-            </div>
-        </div>
-        <!-- Team member-->
-        <div class="col-lg-3 col-sm-6">
-            <div class="team-card-style-1 mx-auto">
-                <div class="team-card-inner">
-                    <div class="team-thumb"><img src="img/team/style-1/07.jpg" alt="Team Member Picture" />
-                    </div>
-                    <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-twitter" href="#"><i
-                                class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-github" href="#"><i
-                                class="socicon-github"></i></a><a class="social-btn sb-style-4 sb-stackoverflow"
-                            href="#"><i class="socicon-stackoverflow"></i></a><a class="social-btn sb-style-4 sb-skype"
-                            href="#"><i class="socicon-skype"></i></a></div><a class="team-contact-link"
-                        href="tel:+19871625346"><i class="fe-icon-phone"></i>&nbsp;(+234) 803485976</a><a
-                        class="team-contact-link" href="mailto:info@example.com"><i
-                            class="fe-icon-mail"></i>&nbsp;info@worldbraintechnology.com</a>
-                </div><span class="team-position">Critical Thinker</span>
-                <h4 class="team-name">Brandon Simon</h4>
-            </div>
->>>>>>> 75a2be5b9f5f3d4068e2873daa2ecae75e4236d0
+         
         </div>
         <!-- Team member-->
         <div class="col-lg-3 col-sm-6">
