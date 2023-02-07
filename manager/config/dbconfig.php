d35db79fbd4fa038743f3a1044b7e26e3c7fc7c3
Godson Pius <godsonazubuike15@gmail.com>
2023-02-07 07:42:16 +0100

    Added the cpanel db details

--- manager/config/dbconfig.php
+++ manager/config/dbconfig.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-// define('HOST', 'localhost');
-// define('USERNAME', 'worlgmig_worldbrain');
-// define('PASSWORD', '100%worldbrain');
-// define('DBNAME', 'worlgmig_worldbrain');
-
 define('HOST', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
+define('USERNAME', 'worlgmig_worldbrain');
+define('PASSWORD', '100%worldbrain');
 define('DBNAME', 'worlgmig_worldbrain');
+
+// define('HOST', 'localhost');
+// define('USERNAME', 'root');
+// define('PASSWORD', '');
+// define('DBNAME', 'worlgmig_worldbrain');
 
 $link = mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
 
